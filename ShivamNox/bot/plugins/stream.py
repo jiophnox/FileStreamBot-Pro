2a46e00e8747f82de79362354eff31a30a7ca958
jiophnox <jiophnox@gmail.com>
2025-11-02 18:59:52 +0530

    Update stream.py

--- ShivamNox/bot/plugins/stream.py
+++ ShivamNox/bot/plugins/stream.py
@@ -101,8 +101,8 @@ async def private_receive_handler(c: Client, m: Message):
 
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link = f"http://hivajoy.fwh.is/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        online_link = f"http://hivajoy.fwh.is/dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
         stream_link2 = f"https://filestreambot-pro-ad54.onrender.com/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
         
         msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b> 🖥Stream 2  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WILL NEVER EXPIRE</b>"""
@@ -153,11 +153,12 @@ async def channel_receive_handler(bot, broadcast):
     
     try:
         log_msg = await broadcast.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link = f"http://hivajoy.fwh.is/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        online_link = f"http://hivajoy.fwh.is/dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link2 = f"https://filestreambot-pro-ad54.onrender.com/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
         
         await log_msg.reply_text(
-            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}",
+            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link2}",
             quote=True
         )
         await bot.edit_message_reply_markup(
